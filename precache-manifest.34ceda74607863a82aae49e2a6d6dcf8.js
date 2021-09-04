4d9899303abfd61d33f55cc03efdeb83153ded0d
Hira Nisar <hira98607@gmail.com>
2021-09-03 22:24:53 -0600

    Updates

---
+++ precache-manifest.34ceda74607863a82aae49e2a6d6dcf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e86f3ca6b80cc4e51e5e7e476af1ab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1335d8f74541ae9f558f",
+    "url": "/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1335d8f74541ae9f558f",
+    "url": "/static/js/main.7e604e1f.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
